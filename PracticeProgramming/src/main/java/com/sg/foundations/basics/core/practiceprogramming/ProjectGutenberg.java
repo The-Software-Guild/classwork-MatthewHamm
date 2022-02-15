be45210cb48cf654cd9308ece139269fdb124d9a
Matthew Hamm <matthewhamm00@gmail.com>
2022-02-15 15:32:09 +0000

    Practice programming

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/core/practiceprogramming/ProjectGutenberg.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.basics.core.practiceprogramming;
+
+/**
+ *
+ * @author Matthew Hamm
+ */
+public class ProjectGutenberg {
+    public static void main(String[] args) {
+        System.out.print("Did you know that in 1440 (or thereabouts),");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.print("His invention made it easy to print and");
+        System.out.println("distribute books to anyone who wanted one.");
+        System.out.print("We are like a modern Gutenberg,");
+        System.out.print("printing vast amounts to the waiting console with ease.");
+    }
+}
