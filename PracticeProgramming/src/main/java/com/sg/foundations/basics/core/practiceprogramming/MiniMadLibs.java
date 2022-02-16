aefa7f6e368b7590c82541f22a17b4b7af1920e3
Matthew Hamm <matthewhamm00@gmail.com>
2022-02-16 10:18:46 +0000

    Added practice programming 2 and started 3

--- PracticeProgramming/src/main/java/com/sg/foundations/basics/core/practiceprogramming/MiniMadLibs.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/core/practiceprogramming/MiniMadLibs.java
@@ -16,26 +16,39 @@ public class MiniMadLibs {
     String adjective1;
     String noun2;
     String number;
-    String adjective;
+    String adjective2;
     String pluralNoun1;
     String pluralNoun2;
     String pluralNoun3;
     String presentVerb;
     String pastVerb;
     
-    Scanner sc =new Scanner(System.in);
+
     
-    System.out.println("Let's play MAD LIBS! \n");
+    System.out.println("Let's play MAD LIBS");
+    System.out.println("");
     System.out.print("I need a noun: ");
-    noun1=sc.nextLine();
-    System.out.print("Now an adjective: ");
-    adjective1=sc.nextLine();
-    System.out.print("Another noun: ");
-    noun2=sc.nextLine();
-    System.out.print("And a number: ");
-    number=sc.nextLine();
-    System.out.print("Another adjective :");
-    adjective=sc.nextLine();
-    
+    Scanner sc =new Scanner(System.in);
+    noun1=sc.next();
+    System.out.print("\nNow an adjective: ");
+    adjective1=sc.next();
+    System.out.print("\nAnother noun: ");
+    noun2=sc.next();
+    System.out.print("\nAnd a number: ");
+    number=sc.next();
+    System.out.print("\nAnother adjective :");
+    adjective2=sc.next();
+    System.out.print("\nA plural noun :");
+    pluralNoun1=sc.next();
+    System.out.print("\nAnother one");
+    pluralNoun2=sc.next();
+    System.out.print("\nOne more: ");
+    pluralNoun3=sc.next();
+    System.out.print("\nA verb (infinitive form): ");
+    presentVerb=sc.next();
+    System.out.print("\nSame verb (past participle): ");
+    pastVerb=sc.next();
+    System.out.println("\n *** NOW LETS GET MAD (libs) ***");
+    System.out.println(noun1+": the "+adjective1+" frontier. These are the voyages of the starship "+ noun2 +". Its "+number+"-year mission: to explore strange "+adjective2+" "+pluralNoun1+", to seek out "+adjective2+" "+pluralNoun2+" and "+adjective2+" "+pluralNoun3+", to boldly "+presentVerb+" where no one has "+pastVerb+" before.");
     }
 }
